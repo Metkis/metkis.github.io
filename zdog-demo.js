8a65a89f55e5e98973567794b9ce6f726680bcfc
Metkis <jhostetler90@gmail.com>
2020-05-05 21:29:58 -0400

    Updated Content
    Changes to stylesheet, added links out, cleaned up documents.

--- zdog-demo.js
+++ zdog-demo.js
@@ -12,9 +12,8 @@ let curve = new Zdog.Ellipse({
   diameter: 80,
   quarters: 2,
   stroke: 20,
-translate: { y: -20 },
-scale: 0.5,
-
+  translate: { y: -20 },
+  scale: 0.5,
   color: '#fff',
 });
 
@@ -24,21 +23,16 @@ curve.copy({
   color: '#fff',
 });
 
-
-
 let stem = new Zdog.Shape({
   addTo: illo,
-scale: 0.5,
-
+  scale: 0.5,
   path: [
     { x: -60,y: -80 }, // start at 1st point
     { x:  0, y: -80}, // line to 2nd point
   ],
   stroke: 20,
   color: '#fff',
-
 });
-
 
 new Zdog.Cone({
   addTo: illo,
@@ -46,14 +40,9 @@ new Zdog.Cone({
   length: 15,
   color: '#c8ffb0',
   backface: '#8ecc72',
-  
-	translate: { z: -60, x:90, y:-60 },
-
-  	rotate:{y:-1.55}
-
+  translate: { z: -60, x:90, y:-60 },
+  rotate:{y:-1.55}
 });
-
-
 
 new Zdog.Cone({
   addTo: illo,
@@ -61,11 +50,8 @@ new Zdog.Cone({
   length: 15,
   color: '#ff9e9e',
   backface: '#db7474',
-  
-	translate: { z: -97.5, x:55, y:-60 },
-	rotate:{x:3}
-
-  
+  translate: { z: -97.5, x:55, y:-60 },
+  rotate:{x:3}
 });
 
 new Zdog.Cone({
@@ -74,15 +60,9 @@ new Zdog.Cone({
   length: 15,
   color: '#9ee2ff',
   backface: '#6cb8d9',
-  
-
-	translate: { z: -60, x:55, y:-97.5 },
-	rotate:{x:1.55}
-
-  
+  translate: { z: -60, x:55, y:-97.5 },
+  rotate:{x:1.55}
 });
-
-
 
 new Zdog.Box({
   addTo: illo,
@@ -99,52 +79,41 @@ new Zdog.Box({
   topFace: false,
   bottomFace: '#9ee2ff',
   frontFace: '#ff9e9e',
-  	translate: { z: -67.5, x:62.5, y:-67.5 },
-
+  translate: { z: -67.5, x:62.5, y:-67.5 },
 });
 
 let ystem = new Zdog.Shape({
   addTo: illo,
-scale: 0.5,
-
+  scale: 0.5,
   path: [
     { x: 110,y: -120, z: -120 }, // start at 1st point
     { x:  180, y: -120, z: -120}, // line to 2nd point
   ],
   stroke: 3,
   color: '#c8ffb0',
-
 });
-
 
 let xstem = new Zdog.Shape({
   addTo: illo,
-scale: 0.5,
-
+  scale: 0.5,
   path: [
     { x: 110,y: -120, z: -120 }, // start at 1st point
     { x:  110, y: -190, z: -120}, // line to 2nd point
   ],
   stroke: 3,
   color: '#9ee2ff',
-
 });
-
 
 let zstem = new Zdog.Shape({
   addTo: illo,
-scale: 0.5,
-
+  scale: 0.5,
   path: [
     { x: 110,y: -120, z: -120 }, // start at 1st point
     { x:  110, y: -120, z: -190}, // line to 2nd point
   ],
   stroke: 3,
   color: '#ff9e9e',
-
 });
-
-
 
 stem.copy({
   // overwrite original options
@@ -166,20 +135,7 @@ new Zdog.Box({
   stroke: 2,
   fill: false,
   color: '#fff',
-    translate: { x: -5 },
-
-
-});
-
-
-new Zdog.Shape({
-  addTo: illo,
-  // no path set, default to single point
-  stroke: 5,
-  color: '#fff',
-  
-      translate: {  x:30, z:30 },
-
+  translate: { x: -5 },
 });
 
 new Zdog.Shape({
@@ -187,20 +143,23 @@ new Zdog.Shape({
   // no path set, default to single point
   stroke: 5,
   color: '#fff',
-  
-      translate: {  x:-30, z:-30 },
-
+  translate: {  x:30, z:30 },
 });
-
 
 new Zdog.Shape({
   addTo: illo,
   // no path set, default to single point
   stroke: 5,
   color: '#fff',
-  
-        translate: {  x:-30, z:-30 },
+  translate: {  x:-30, z:-30 },
+});
 
+new Zdog.Shape({
+  addTo: illo,
+  // no path set, default to single point
+  stroke: 5,
+  color: '#fff',
+  translate: {  x:-30, z:-30 },
 });
 
 new Zdog.Shape({
@@ -208,20 +167,15 @@ new Zdog.Shape({
   // no path set, default to single point
   stroke: 3,
   color: '#fff',
-  
-        translate: {  x:-20, z:-50 },
-
+  translate: {  x:-20, z:-50 },
 });
-
 
 new Zdog.Shape({
   addTo: illo,
   // no path set, default to single point
   stroke: 7,
   color: '#fff',
-  
-        translate: {  x:35, z:20 , y: -10},
-
+  translate: {  x:35, z:20 , y: -10},
 });
 
 
@@ -230,9 +184,7 @@ new Zdog.Shape({
   // no path set, default to single point
   stroke: 5,
   color: '#fff',
-  
-      translate: {  x:-30, z:30 , y: 45},
-
+  translate: {  x:-30, z:30 , y: 45},
 });
 
 new Zdog.Shape({
@@ -240,9 +192,7 @@ new Zdog.Shape({
   // no path set, default to single point
   stroke: 5,
   color: '#fff',
-  
-      translate: {  x:20, z:-40 ,y:40},
-
+  translate: {  x:20, z:-40 ,y:40},
 });
 
 
@@ -251,9 +201,7 @@ new Zdog.Shape({
   // no path set, default to single point
   stroke: 5,
   color: '#fff',
-  
-        translate: {  x:-10, z:-30, y:-30 },
-
+  translate: {  x:-10, z:-30, y:-30 },
 });
 
 new Zdog.Shape({
@@ -261,9 +209,7 @@ new Zdog.Shape({
   // no path set, default to single point
   stroke: 3,
   color: '#fff',
-  
-        translate: {  x:-20, z:20, y:30 },
-
+  translate: {  x:-20, z:20, y:30 },
 });
 
 new Zdog.Shape({
@@ -271,9 +217,7 @@ new Zdog.Shape({
   // no path set, default to single point
   stroke: 3,
   color: '#fff',
-  
-        translate: {  x:-40, z:40, y:40 },
-
+  translate: {  x:-40, z:40, y:40 },
 });
 
 
@@ -282,9 +226,7 @@ new Zdog.Shape({
   // no path set, default to single point
   stroke: 3,
   color: '#fff',
-  
-        translate: {  x:40, z:30, y:40 },
-
+  translate: {  x:40, z:30, y:40 },
 });
 
 new Zdog.Shape({
@@ -292,9 +234,7 @@ new Zdog.Shape({
   // no path set, default to single point
   stroke: 3,
   color: '#fff',
-  
-        translate: {  x:40, z:-40, y:-40 },
-
+  translate: {  x:40, z:-40, y:-40 },
 });
 
 new Zdog.Shape({
@@ -302,9 +242,7 @@ new Zdog.Shape({
   // no path set, default to single point
   stroke: 4,
   color: '#fff',
-  
-        translate: {  x:-40, z:40, y:-40 },
-
+  translate: {  x:-40, z:40, y:-40 },
 });
 
 new Zdog.Shape({
@@ -312,20 +250,15 @@ new Zdog.Shape({
   // no path set, default to single point
   stroke: 3,
   color: '#fff',
-  
-        translate: {  x:-50, z:0, y:-10 },
-
+  translate: {  x:-50, z:0, y:-10 },
 });
 
 function animate() {
-
   illo.rotate.z = 4.70;
   illo.rotate.y += 0.03;
   illo.rotate.x = -0.45;
-
   requestAnimationFrame( animate );
-    illo.updateRenderGraph();
-
+  illo.updateRenderGraph();
 }
 
 animate();
